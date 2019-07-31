e147f796922d6df02adacaa28a03431872f40642
Tomasz Fiechowski <tomasz.fiechowski@gmail.com>
2019-08-01 01:31:11 +0700

    Make Button clickable, add tests

--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -1,4 +1,4 @@
-import { render, getByText } from "@testing-library/react";
+import { render, getByText, fireEvent } from "@testing-library/react";
 import React from "react";
 import Button from "components/Button";
 
@@ -7,5 +7,17 @@ describe("Button", () => {
     const { container } = render(<Button text="We Salute You!" />);
 
     getByText(container, "We Salute You!");
+  });
+
+  test("should handle click events", () => {
+    const onClickMock = jest.fn();
+    const { container } = render(
+      <Button text="Click me, maybe?" onClick={onClickMock} />
+    );
+    const component = container.firstChild;
+
+    fireEvent.click(component);
+
+    expect(onClickMock).toBeCalled();
   });
 });
