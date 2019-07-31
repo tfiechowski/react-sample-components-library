d0a32b1d5b6cb69b6a93d9b2f303ef25cc7cab85
Tomasz Fiechowski <tomasz.fiechowski@gmail.com>
2019-07-31 17:42:19 +0700

    Extract shared styles from Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,11 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { font, primaryColors, shape } from "../config/styles";
 
 const Wrapper = styled.button`
-  text-transform: uppercase;
-  font-size: 1.5em;
-  font-weight: bold;
-  letter-spacing: 4px;
-  background: #5cdb95;
-  color: #05385b;
-  border: none;
-  border-radius: 5px;
-  padding: 10px 20px;
+  ${font}
+  ${primaryColors}
+  ${shape}
 `;
 
 export default function Button({ text }) {
---
+++ src/config/styles.js
@@ -0,0 +1,19 @@
+import { css } from "@emotion/core";
+
+export const font = css`
+  text-transform: uppercase;
+  font-size: 1.5em;
+  font-weight: bold;
+  letter-spacing: 4px;
+`;
+
+export const shape = css`
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+`;
+
+export const primaryColors = css`
+  background: #5cdb95;
+  color: #05385b;
+`;
